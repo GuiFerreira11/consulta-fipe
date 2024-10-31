111fe7c5732a490ce34f8b7e4352bf20f2d01ec2
Gui Ferreira <guifl_pereira@hotmail.com>
2024-10-31 19:44:22 -0300

    Iniciando novo desafio criando uma aplicação para consulta a tabela FIPE.

---
+++ src/test/java/com/example/consultafipe/ConsultafipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consultafipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultafipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
